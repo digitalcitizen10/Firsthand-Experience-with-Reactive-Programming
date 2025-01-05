24c79ea86fb012981816b36b9e71405d3932368a
Amirreza-DigitalCitizen10 <me@amrrza.com>
2025-01-05 09:05:58 +0330

    Initial commit: Spring Boot project setup

---
+++ src/test/java/ir/codecleanic/reactiveprogrammingtutorial/ReactiveprogrammingtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.codecleanic.reactiveprogrammingtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveprogrammingtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
